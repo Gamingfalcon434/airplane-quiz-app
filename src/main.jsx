26203aba78f858902ee2a0def36667354309ef37
Gamingfalcon434 <zacharycmatthews@icloud.com>
2025-01-26 21:52:41 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import USFighterJetQuiz from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <USFighterJetQuiz />
+  </React.StrictMode>
+);
